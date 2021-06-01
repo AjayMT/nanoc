b0808dfffe01ce244f6eb245d13e7aaf0d5b3937
Ajay MT <ajay.tatachar@gmail.com>
2021-05-31 23:40:17 -0500

    [wip] add data symbols

--- test/archive.c
+++ test/archive.c
@@ -1,8 +1,11 @@
 
+extern int foo;
+
 void _start();
 
 int main(int a, int b, int c)
 {
+  foo = 1;
   _start();
   return (int)_start;
 }
--- test/hello.c
+++ test/hello.c
@@ -1,6 +1,8 @@
 
 int main(int a, int b, int c);
 
+int b;
+
 void _start()
 {
   main(1, 2, 3);
